46c978ad9875932fb973555d53053b148f8c3a79
Oxakromax <nhidalgos@uft.edu>
2023-05-14 05:13:31 -0400

    Process Control

---
+++ lib/pages/Dual/Processes/processes_view_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProcessesViewPage extends StatefulWidget {
+  const ProcessesViewPage({Key key}) : super(key: key);
+
+  @override
+  _ProcessesViewPageState createState() => _ProcessesViewPageState();
+}
+
+class _ProcessesViewPageState extends State<ProcessesViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
